d902840d17d3bdfe6c9eed7bd8b01a9bc6e9615d
ThanhTu130200 <thanhtukr1302@gmail.com>
2022-05-14 21:40:46 +0700

    change hover transition of quotes area

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Navbar, Nav } from "react-bootstrap"
+import { Navbar, Nav, DropdownButton, Dropdown } from "react-bootstrap"
 import { LinkContainer } from "react-router-bootstrap"
 
 import "./Nav.scss"
--- src/components/QuotesArea/QuotesArea.js
+++ src/components/QuotesArea/QuotesArea.js
@@ -20,6 +20,7 @@ function QuotesArea() {
 				{visibleItems
 					? visibleItems.map((item, index) => (
 							<LinkContainer
+								key={index}
 								to={`store/item-${item.id}`}
 								style={{ backgroundImage: `url(${item.image})` }}>
 								<div className="quote">
@@ -37,72 +38,25 @@ function QuotesArea() {
 			<div className="quotesArea__mobile d-block d-sm-none">
 				<Container>
 					<Row xs={1}>
-						<Col
-							className="quote"
-							style={{
-								backgroundImage:
-									"url('https://media.voguebusiness.com/photos/621e06bf17b9c2e9b062bc0b/master/pass/reliance-acquisition-voguebus-full-getty-credit-mar-22-story.jpg')",
-							}}>
-							<div className="content">
-								<h1>Heather grey basics</h1>
-								<p>New Arrival</p>
-							</div>
-						</Col>
-						<Col
-							className="quote"
-							style={{
-								backgroundImage:
-									"url('https://cdnimg.vietnamplus.vn/t1200/Uploaded/fsmsr/2022_03_07/hoa_hau_luong_thuy_linh_8.png')",
-							}}>
-							<div className="content">
-								<h1>Heather grey basics</h1>
-								<p>New Arrival</p>
-							</div>
-						</Col>
-						<Col
-							className="quote"
-							style={{
-								backgroundImage:
-									"url('https://media.voguebusiness.com/photos/621e06bf17b9c2e9b062bc0b/master/pass/reliance-acquisition-voguebus-full-getty-credit-mar-22-story.jpg')",
-							}}>
-							<div className="content">
-								<h1>Heather grey basics</h1>
-								<p>New Arrival</p>
-							</div>
-						</Col>
-						<Col
-							className="quote"
-							style={{
-								backgroundImage:
-									"url('https://cdnimg.vietnamplus.vn/t1200/Uploaded/fsmsr/2022_03_07/hoa_hau_luong_thuy_linh_8.png')",
-							}}>
-							<div className="content">
-								<h1>Heather grey basics</h1>
-								<p>New Arrival</p>
-							</div>
-						</Col>
-						<Col
-							className="quote"
-							style={{
-								backgroundImage:
-									"url('https://media.voguebusiness.com/photos/621e06bf17b9c2e9b062bc0b/master/pass/reliance-acquisition-voguebus-full-getty-credit-mar-22-story.jpg')",
-							}}>
-							<div className="content">
-								<h1>Heather grey basics</h1>
-								<p>New Arrival</p>
-							</div>
-						</Col>
-						<Col
-							className="quote"
-							style={{
-								backgroundImage:
-									"url('https://cdnimg.vietnamplus.vn/t1200/Uploaded/fsmsr/2022_03_07/hoa_hau_luong_thuy_linh_8.png')",
-							}}>
-							<div className="content">
-								<h1>Heather grey basics</h1>
-								<p>New Arrival</p>
-							</div>
-						</Col>
+						{visibleItems
+							? visibleItems.map((item, index) => (
+									<LinkContainer
+										key={index}
+										to={`store/item-${item.id}`}
+										style={{
+											backgroundImage: `url(${item.image})`,
+										}}>
+										<Col className="quote">
+											<div className="content">
+												<h1 className="name">{item.name}</h1>
+												<p className="price">
+													{convertCurrency(item.price)}
+												</p>
+											</div>
+										</Col>
+									</LinkContainer>
+							  ))
+							: ""}
 					</Row>
 				</Container>
 			</div>
