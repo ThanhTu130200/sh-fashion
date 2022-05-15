b50476f69e6d781f789b7e301dfa8b60b37cc6df
ThanhTu130200 <thanhtukr1302@gmail.com>
2022-05-15 11:29:55 +0700

    change api URL

--- src/contents/Categories.js
+++ src/contents/Categories.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export default async function Categories() {
 	return await axios
-		.get("https://627cc7abe5ac2c452af68326.mockapi.io/sh-fashion")
+		.get("https://627cc7abe5ac2c452af68326.mockapi.io/categories")
 		.then((res) => res.data)
 		.catch((err) => alert(err.message))
 }
