7acc6ffba402f1cb3ab286927abdbca790ce1d56
ThanhTu130200 <thanhtukr1302@gmail.com>
2022-05-25 14:53:52 +0700

    restyling order history page

--- src/pages/OrderHistory/OrderHistory.js
+++ src/pages/OrderHistory/OrderHistory.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import { useSelector } from "react-redux"
+import { LinkContainer } from "react-router-bootstrap"
 
 import Orders from "../../components/Orders/Orders"
 
@@ -11,22 +12,39 @@ function OrderHistory() {
 
 	return (
 		<div className="orderHistoryPage">
-			<h1 className="text-center fs_26 fw-bold orderHistoryPage__header">
-				THIS IS YOUR 10 LAST ORDERS
-			</h1>
+			{orderHistory.length > 0 ? (
+				<>
+					<div className="orderHistoryPage__header">
+						<h1 className="text-center fs_26 fw-bold">THIS IS YOUR 10 LAST ORDERS</h1>
+					</div>
 
-			{show ? (
-				<Orders orderHistory={orderHistory.slice(0, 10)} />
+					{show ? (
+						<Orders orderHistory={orderHistory.slice(0, 10)} />
+					) : (
+						<Orders orderHistory={orderHistory.slice(0, 3)} />
+					)}
+					{orderHistory.length > 3 ? (
+						<div
+							className="text-center fs_10 cp showMoreBtn"
+							onClick={() => {
+								setShow(!show)
+							}}>
+							{show ? "SHOW ME LESS ORDERS" : "SHOW ME MORE ORDERS"}
+						</div>
+					) : (
+						""
+					)}
+				</>
 			) : (
-				<Orders orderHistory={orderHistory.slice(0, 3)} />
+				<div className="orderHistoryPage__header">
+					<h1 className="text-center fs_26 fw-bold">YOUR ORDER HISTORY IS EMPTY</h1>
+					<LinkContainer to="/store">
+						<p className="text-center fst-italic text-decoration-underline cp c_primary keepShopping">
+							Keep shopping
+						</p>
+					</LinkContainer>
+				</div>
 			)}
-			<div
-				className="text-center fs_10 cp showMoreBtn"
-				onClick={() => {
-					setShow(!show)
-				}}>
-				{show ? "SHOW ME LESS ORDERS" : "SHOW ME MORE ORDERS"}
-			</div>
 		</div>
 	)
 }
